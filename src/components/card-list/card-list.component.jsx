9be1c49085edeed27471873429e9bc6b920884f7
Ankit Singh <kislotaooankit@gmail.com>
2022-07-20 19:07:43 +0530

    Implemented via class componenents

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import Card from "../card/card.component";
+
+import "./card-list.styles.css";
+
+class CardList extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    console.log("Cardlist rendering");
+    const monsters = this.props.monsters;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
